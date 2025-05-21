7385f5c39236e0439b3c00ad1d41db2fc3d52640
alexishs <a.halbotschoonaert@gmail.com>
2025-05-21 09:49:56 +0200

    feat: :sparkles: DBConnection : when creating new record, PK whose value will be set by SQLite is initialized to None

--- test_class_db.py
+++ test_class_db.py
@@ -1,16 +1,16 @@
-from src.db_api import DBConnection, DBTableRecord
+from src.db_api import DBConnection
 
 ma_connexion = DBConnection('online_bikes.db')
 ma_connexion.executescript('src/db.sql')
-mon_enreg = ma_connexion.new_table_record('Product', {'product_Id' : 1}, False)
+mon_enreg = ma_connexion.new_table_record('Product', {'product_Id' : 2}, False)
 mon_enreg.set_field('description', 'voici ma nouvelle description.')
 mon_enreg.save_record()
 
-mon_enreg = ma_connexion.new_table_record('Product', {'product_Id' : 'AUTO'}, True)
+mon_enreg = ma_connexion.new_table_record('Product', {'product_Id' : None}, True)
 mon_enreg.set_field('description', 'Ceci est un nouveau produit.')
 mon_enreg.set_field('price', 123)
 mon_enreg.save_record()
 
-ma_connexion.delete_record('Product', {'product_Id' : 1})
+ma_connexion.delete_record('Product', {'product_Id' : 2})
 
 ma_connexion.commit()
