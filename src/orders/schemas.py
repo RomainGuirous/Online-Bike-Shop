a23836be50a80e4981ce0807d02962551e0687af
alexishs <a.halbotschoonaert@gmail.com>
2025-05-22 10:13:58 +0200

    refactor: :art: Remove old file orders/schemas.py

--- src/orders/schemas.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel, Field
-
-class OrderBase(BaseModel):
-    order_id: int = Field(..., description="Unique identifier for the order")
-    user_id: int = Field(..., description="Unique identifier for the user")
-    product_id: int = Field(..., description="Unique identifier for the product")
-    quantity: int = Field(..., description="Quantity of the product ordered")
