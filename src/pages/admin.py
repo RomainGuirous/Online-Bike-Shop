11ed8450f1040137914d6618fcd390306a2222fe
alexishs <a.halbotschoonaert@gmail.com>
2025-05-23 15:55:09 +0200

    refactor: :art: access right management refactored in one function

--- src/pages/admin.py
+++ src/pages/admin.py
@@ -2,26 +2,8 @@ import streamlit as st
 import streamlit_authenticator as stauth
 from yaml import SafeLoader
 import yaml
+import streamlit_utils as st_utils
 
 st.set_page_config(page_title="Admin", page_icon="🛠️")
 
-
-with open('config.yaml') as file:
-    config = yaml.load(file, Loader=SafeLoader)
-
-authenticator = stauth.Authenticate(
-    config['credentials'],
-    config['cookie']['name'],
-    config['cookie']['key'],
-    config['cookie']['expiry_days']
-)
-
-if "admin" not in (st.session_state.get("roles") or []):
-    st.error("Access denied. You are not an admin.")
-    if st.button("Login"):
-        st.switch_page("pages/connection.py")
-
-    
-if st.session_state.get('authentication_status'):
-    st.success(f'Welcome {st.session_state["name"]}!')
-    authenticator.logout()
+st_utils.handle_access_rights('admin')
