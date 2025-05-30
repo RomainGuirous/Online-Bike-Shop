8b4b6a46b53ed5fae6aa4341404936f55cb0659d
Florian <florian.abgrall750@gmail.com>
2025-05-30 14:16:21 +0200

    feat: :sparkles: use user models for updating user details

--- src/pages/update_user.py
+++ src/pages/update_user.py
@@ -4,6 +4,9 @@ import yaml
 from yaml.loader import SafeLoader
 import streamlit_utils as st_utils
 from style.style import get_card_style
+from users.models import User
+from db_api import create_connection
+
 
 st.set_page_config(page_title="modifier utilisateur")
 get_card_style()
@@ -24,8 +27,16 @@ authenticator = stauth.Authenticate(
 if st.session_state.get("authentication_status"):
     try:
         if authenticator.update_user_details(st.session_state.get("username")):
-            st.success("Entries updated successfully")
             with open("config.yaml", "w") as file:
                 yaml.dump(config, file, default_flow_style=False, allow_unicode=True)
+            conn = create_connection()
+            user = config["credentials"]["usernames"][st.session_state.get("username")]
+            user_model = User(db_connection=conn, is_new=False, user_id=st.session_state.get("user_id"))
+            user_model.first_name = user["first_name"]
+            user_model.last_name = user["last_name"]
+            user_model.email = user["email"]
+            user_model.save_to_db()
+            conn.commit()
+            st.success("Entries updated successfully")
     except Exception as e:
         st.error(e)
