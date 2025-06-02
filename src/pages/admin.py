90a22edbbaa868586ae075894f4902bc2411183c
Florian <florian.abgrall750@gmail.com>
2025-06-02 09:11:20 +0200

    feat: :sparkles: update user to db

--- src/pages/admin.py
+++ src/pages/admin.py
@@ -21,7 +21,12 @@ tabs = st.tabs(["📦 Orders", "🛍️ Products", "👤 Users"])
 
 order_df = get_order_list(conn)
 product_df = get_product_dataframe(conn)
-user_df = get_user_list(conn)
+user_list = get_user_list(conn)
+user_df = pd.DataFrame(user_list)
+
+st.dataframe(order_df)
+st.dataframe(product_df)
+st.dataframe(user_df)
 
 product_df["price"] = (
     product_df["price"].astype(str).str.replace(r"[^\d\.\-]", "", regex=True)
